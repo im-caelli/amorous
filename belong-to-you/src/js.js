5f6df10b4918f1de6e47d55650a26024906da239
im-caelli <im.caelli@gmail.com>
2020-09-12 02:34:35 -0400

    Dark Mode

--- belong-to-you/src/js.js
+++ belong-to-you/src/js.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
+  // Back to Top
   $('#back-to-top').hide();
 
-  // Back to Top
   $(window).scroll(function() {
 		if($(this).scrollTop() != 0) {
 			$('#back-to-top').fadeIn("slow");
@@ -24,5 +24,17 @@ $(document).ready(function(){
      document.cookie = 'modal_shown=seen'; //set cookie modal_shown
      //cookie will expire when browser is closed
    }
+
+   // Dark Mode
+
+   $('.dm-btn').on('click', function() {
+     $('body').toggleClass('dark-mode');
+
+     if ($('body').hasClass('dark-mode')) {
+      $('.dm-btn .fas').removeClass('fa-moon').addClass('fa-sun');
+     } else {
+      $('.dm-btn .fas').removeClass('fa-sun').addClass('fa-moon');
+     }
+   })
 
 });
